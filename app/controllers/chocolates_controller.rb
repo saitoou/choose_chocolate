11c837bd041ea4063f58208b6bc7ee1bbfc54141
saitoou <kouhei02.3110@gmail.com>
2021-10-29 00:27:09 +0900

    Fix:pathのミス修正

--- app/controllers/chocolates_controller.rb
+++ app/controllers/chocolates_controller.rb
@@ -1,6 +1,6 @@
 class ChocolatesController < ApplicationController
 # chocolate一覧を見るためのcontroller
   def index
-    @chocolates_list = Chocolate.joins(:review).all.select("name", "detail", "reviews.*")
+    @chocolates_list = Chocolate.joins(:review).all.select("name", "detail", "image", "reviews.*")
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   get 'privacy', to: 'static_pages#privacy'
   get 'terms', to: 'static_pages#terms'
   #stress_check_page
-  get 'choice', to: 'chocolate_choice#new'
+  get 'choice', to: 'chocolate_choices#new'
   #recommend_chocolate_page 
   get 'stress_free_chocos', to: 'stress_free_chocos#new'
   get 'good_stimulus_chocos', to: 'good_stimulus_chocos#new'
   get 'tired_chocos', to: 'tired_chocos#new'
   get 'exhausted_chocos', to: 'exhausted_chocos#new'
   #chocolates list
-  get 'choicolates_list', to:  'chocolates#index'
+  get 'chocolates_list', to:  'chocolates#index'
   
 end
