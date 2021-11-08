8ce80e0a9587f514a442f3727a718aad7b6ae905
saitoou <kouhei02.3110@gmail.com>
2021-11-08 21:28:10 +0900

    initial commit

--- app/controllers/exhausted_chocos_controller.rb
+++ app/controllers/exhausted_chocos_controller.rb
@@ -1,5 +1,8 @@
 class ExhaustedChocosController < ApplicationController
   def new
     @exhausted_choco = Chocolate.joins(:review).merge(Review.where(sweet:5)).take(1)
+    #  レーダーチャートで示したいので取得
+    @chocolate = @exhausted_choco.ids
+    @review = Review.find(@chocolate)
   end
 end
--- app/controllers/good_stimulus_chocos_controller.rb
+++ app/controllers/good_stimulus_chocos_controller.rb
@@ -1,5 +1,8 @@
 class GoodStimulusChocosController < ApplicationController
   def new
     @good_stimulus_choco = Chocolate.joins(:review).merge(Review.where(appearance:4, texture:4)).take(1)
+    #  レーダーチャートで示したいので取得
+    @chocolate = @good_stimulus_choco.ids
+    @review = Review.find(@chocolate)
   end
 end
