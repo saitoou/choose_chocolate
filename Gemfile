f4631a0efe4d2d05c16bd591edc1f5f6331d82de
saitoou <kouhei02.3110@gmail.com>
2022-01-03 18:08:52 +0900

    画像の保存方法の追加

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'rails-i18n'
 gem 'seed-fu'
 gem 'sorcery'
+gem 'mini_magick'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/chocolates_controller.rb
+++ app/controllers/chocolates_controller.rb
@@ -1,6 +1,7 @@
 class ChocolatesController < ApplicationController
 # chocolate一覧を見るためのcontroller
   def index
-    @chocolates_list = Chocolate.joins(:review).all.select("name", "detail", "image", "reviews.*")
+    # @chocolates_list = Chocolate.joins(:review).all.select("name", "detail", "image", "reviews.*")
+    @chocolate_lists = Chocolate.all
   end  
 end
