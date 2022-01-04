c76049c7db5e55516ee4406825dc095f0249d25b
saitoou <kouhei02.3110@gmail.com>
2022-01-04 20:24:46 +0900

    meira-

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://choosechocolate.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    authentication: 'plain',
+    user_name: Rails.application.credentials.gmail[:email],
+    password: Rails.application.credentials.gmail[:password],
+    domain: 'gmail.com',
+    enable_starttls_auto: true
+  }
+
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
