fc4ea72f008a6b33314bca6ad666f64bc8db988a
saitoou <kouhei02.3110@gmail.com>
2022-01-09 16:50:11 +0900

    fix babel config

--- babel.config.js
+++ babel.config.js
@@ -66,12 +66,6 @@ module.exports = function(api) {
         {
           async: false
         }
-      ],
-      [
-        '@babel/plugin-proposal-private-methods',
-         {
-           loose: true 
-         }
       ]
     ].filter(Boolean)
   }
