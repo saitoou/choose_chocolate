bd64018d856b9c5cdb3e8956656cb700b725f3d5
saitoou <kouhei02.3110@gmail.com>
2022-02-19 00:31:12 +0900

    admin user list

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,32 @@
+class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[edit update destroy]
+  def index
+    @users = User.all
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path
+      flash[:success] = "ユーザー情報を変更しました"
+    else
+      flash.now[:danger] = "ユーザー情報を変更できませんでした"
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy!
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :passward, :password_confirmation, :role)
+  end
+end
